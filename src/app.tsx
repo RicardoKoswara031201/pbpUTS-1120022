4c41623c1e79a58184addba2c7fa1130343f8586
Ricardo K <rikokwee08@gmail.com>
2026-02-27 10:05:16 +0700

    Initial template for UTS

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <h1>UTS</h1>
+      
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
